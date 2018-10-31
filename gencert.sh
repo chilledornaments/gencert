4b6ef74e69afd142b7a85ee98928e59a1bcc2ea1
Mitch <mitchell.anderson@colorado.edu>
2018-10-31 12:27:46 -0600

    Update gencert.sh

--- gencert.sh
+++ gencert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-read -p "Enter Server Name (i.e. elk.imm.com): " servername
+read -p "Enter Server Name (i.e. elk.example.com): " servername
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
     -subj "/C=US/ST=CO/L=Boulder/O=IT/CN=${servername}" \
     -keyout "${servername}".key  -out "${servername}".cert
