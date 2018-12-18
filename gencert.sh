f88cb51653fab82898607c4cf58c64b2a1239f6d
Mitch <mitchell.anderson@colorado.edu>
2018-12-18 14:14:40 -0700

    make it a ten year cert

--- gencert.sh
+++ gencert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "Enter Server Name (i.e. elk.example.com): " servername
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
+openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
     -subj "/C=US/ST=CO/L=Boulder/O=IT/CN=${servername}" \
     -keyout "${servername}".key  -out "${servername}".cert
